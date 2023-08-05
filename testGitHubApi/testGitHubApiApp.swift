d8744a7e0a3a5d1a8c4f3c7fe313e580d12a057e
Dmitry Aliyev <dmitry.y.aliev@raiffeisen.ru>
2023-08-05 22:26:31 +0200

    Initial Commit

---
+++ testGitHubApi/testGitHubApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGitHubApiApp.swift
+//  testGitHubApi
+//
+//  Created by ALIEV Dmitry on 05.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct testGitHubApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
